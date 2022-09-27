e878d9a5515da334c9b147124bdb6a02d66fef50
Daniel Arakcheev <danielarakcheev@Daniels-MacBook-Pro.local>
2022-09-26 21:32:30 -0400

    121 done

---
+++ easy/121_buy_sell_stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        lowest = prices[0]
+        maxProfit = 0
+        for v in prices:
+            lowest = min(lowest,v)
+            profit = v-lowest
+            if profit > maxProfit:
+                maxProfit = profit
+        return maxProfit
+            
