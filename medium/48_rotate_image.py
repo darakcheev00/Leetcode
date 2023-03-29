1afa69041b35fcefbfd560e0674fc2830a7a0f1c
Daniel Arakcheev <darakche@uwaterloo.ca>
2023-03-28 22:31:15 -0400

    48 done

---
+++ medium/48_rotate_image.py
@@ -0,0 +1,32 @@
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        # flip diagonally
+        for i in range(n):
+            for j in range(i+1,n):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = temp
+
+        # flip vertically
+        for i in range(n):
+            for j in range(n//2):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[i][n-j-1]
+                matrix[i][n-j-1] = temp
+
+"""
+idea:
+1) flip diagonally
+2) flip vertically
+
+or 
+
+1) flip horizontally
+2) flip diagonally
+"""
